ecc5861ace6869b44483a2ec725805826cb98a70
devrihan <rihandav2004@gmail.com>
2025-08-30 20:18:16 +0530

    back fix20

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"], # Allows all methods (GET, POST, etc.)
     allow_headers=["*"], # Allows all headers
