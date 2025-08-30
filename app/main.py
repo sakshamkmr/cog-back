8a0347f2e2327d720bf204abc7fe139e74789d9e
devrihan <rihandav2004@gmail.com>
2025-08-31 00:18:08 +0530

    bug fix

--- app/main.py
+++ app/main.py
@@ -11,7 +11,8 @@ origins = [
     "http://localhost:5173",  # Your local development frontend
     "http://localhost:3000",  # Another common local port
     # Add your deployed frontend URL here when you have it
-    "https://cog-front.vercel.app"
+    "https://cog-front.vercel.app",
+    "https://cog-front.vercel.app/load-balancer"
 ]
 
 app.add_middleware(
